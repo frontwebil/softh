4e5d2a8091754e999642df0b0a3a3bf126df2152
Illya Sukhomlyn <gollpfd@gmail.com>
2026-08-06 18:54:35 +0300

    update

--- src/Components/Personal/Personal.tsx
+++ src/Components/Personal/Personal.tsx
@@ -1,5 +1,12 @@
 export function Personal() {
   return <section className="personal">
-    <div className="container"></div>
+    <div className="container">
+      <div className="personal-top">
+        <div className="personal-top-left"></div>
+        <div className="personal-top-right">
+          <p>Ви отримуєте зрозумілі пояснення, кілька варіантів лікування та комплексний погляд на складні випадки</p>
+        </div>
+      </div>
+    </div>
   </section>;
 }
