c89d059732dd80a086522dec682e592cf0d863b9
Illya Sukhomlyn <gollpfd@gmail.com>
2026-08-06 19:08:33 +0300

    update

--- src/Components/Personal/Personal.tsx
+++ src/Components/Personal/Personal.tsx
@@ -1,12 +1,26 @@
 export function Personal() {
-  return <section className="personal">
-    <div className="container">
-      <div className="personal-top">
-        <div className="personal-top-left"></div>
-        <div className="personal-top-right">
-          <p>Ви отримуєте зрозумілі пояснення, кілька варіантів лікування та комплексний погляд на складні випадки</p>
+  return (
+    <section className="personal">
+      <div className="container">
+        <div className="personal-top">
+          <div className="personal-top-left">
+            <div className="personal-top-left-line">
+              <div className="line"></div>
+              <p>спеціалісти</p>
+            </div>
+            <h2 className="personal-top-left-title">
+              Команда,{" "}
+              <span style={{ fontStyle: "italic" }}>якій довіряють</span>{" "}
+            </h2>
+          </div>
+          <div className="personal-top-right">
+            <p>
+              Ви отримуєте зрозумілі пояснення, кілька варіантів лікування та
+              комплексний погляд на складні випадки
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 }
