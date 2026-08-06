204de1d7815dda02d3f217b319cce662e5146221
Illya Sukhomlyn <gollpfd@gmail.com>
2026-08-07 02:12:14 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
     <>
       {/* <Header /> */}
       {/* <Hero /> */}
-      {/* <Advantages /> */}
-      {/* <Request /> */}
-      {/* <Services /> */}
+      <Advantages />
+      <Request />
+      <Services />
       <AboutBanner />
       <Personal />
       <Cases />
